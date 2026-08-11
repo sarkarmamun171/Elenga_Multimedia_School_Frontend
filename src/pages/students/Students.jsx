b3a8f5608fadf81fdd04de95154493acfb82fc8f
sarkarmamun171 <sarkar.pau.cse171@gmail.com>
2026-08-11 22:07:14 +0600

    Students Managment Pages add

---
+++ src/pages/students/Students.jsx
@@ -0,0 +1,12 @@
+function Students(){
+    return(
+        <div>
+            <h1 className="text-3xl font-bold">
+                Students
+            </h1>
+            <p className="mt-2 text-gray-600">
+                  Student Management Page
+            </p>
+        </div>
+    );
+}
