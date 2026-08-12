8f49f51bd22c6c5d81932bb26d4af0a8c2c615a1
sarkarmamun171 <sarkar.pau.cse171@gmail.com>
2026-08-12 22:26:21 +0600

    Create Students page header

--- src/pages/students/Students.jsx
+++ src/pages/students/Students.jsx
@@ -1,13 +1,20 @@
 function Students(){
     return(
-        <div>
-            <h1 className="text-3xl font-bold">
-                Students
-            </h1>
-            <p className="mt-2 text-gray-600">
-                  Student Management Page
-            </p>
+       <div>
+        <div className="flex justify-between items-center mb-6">
+            <div>
+                <h1 className="text-3xl font-blod-bold">
+                    Students
+                </h1>
+                <p className="text-gray-500 mt-1">
+                    Manage All Students
+                </p>
+            </div>
+                  <button className="bg-blue-600 text-white px-5 py-2 rounded-lg hover:bg-blue-700">
+                    + Add Student
+                </button>
         </div>
+       </div>
     );
 }
 export default Students;
